01b0f0f5b8f680191d02f1fd03ed56008335a425
bryam <layhamshavii@gmail.com>
2025-04-09 10:25:23 +0000

    bryam todolist

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function App() {
+  // Estado para la tarea actual y la lista de tareas
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+  // Función para agregar una tarea
+  const handleAddTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  // Función para eliminar una tarea
+  const handleDeleteTask = (indexToRemove) => {
+    const newTasks = tasks.filter((_, index) => index !== indexToRemove);
+    setTasks(newTasks);
+  };
 
-export default Home;
+  // Permitir agregar tareas al presionar la tecla Enter
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleAddTask();
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Tarea</h1>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Agregar nueva Tarea"
+            value={task}
+            onChange={(e) => setTask(e.target.value)} onKeyPress={handleKeyPress}
+          />
+          <button onClick={handleAddTask}>Agregar</button>
+        </div>
+        <ul className="task-list">
+          {tasks.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button className="delete-button" onClick={() => handleDeleteTask(index)}>
+                Eliminar
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
